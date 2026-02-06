71c4f8e6ab36524455eaffde4655867c1208bc6b
Kisakye5308 <kisakye.kabazaile@student.utamu.ac.ug>
2026-02-06 16:24:30 +0300

    Remove .next from tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aE4/uZLvxMEmFJy7OC/ODdf+nHMwHARS72/dXJmSrhI=\"\n}"
--- .next/dev/static/chunks/src_app_library_page_tsx_ebb7e3bc._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_078cd9ef._.js"
-],
-    source: "dynamic"
-});
